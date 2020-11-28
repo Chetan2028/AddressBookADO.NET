e333de86550c9e9e5c579bc69ecb5ccf9579ab73
Chetan2028 <bmchetan2028@gmail.com>
2020-11-28 07:58:49 +0530

    [Chetan] refactor . Refactored main method

--- AddressBookADO.NET/Program.cs
+++ AddressBookADO.NET/Program.cs
@@ -8,8 +8,20 @@ namespace AddressBookADO.NET
         {
             Console.WriteLine("Welcome to Address book Service");
             AddressBookRepo addressBookRepo = new AddressBookRepo();
-            //addressBookRepo.RetrieveContactByState();
-            addressBookRepo.RetrieveContactByCity();
+            AddressBookModel model = new AddressBookModel();
+            model.FirstName = "Ayush";
+            model.LastName = "Vedpatak";
+            model.Address = "Near IMA HAll";
+            model.City = "Mysore";
+            model.State = "Karnataka";
+            model.Zip = 410256;
+            model.PhoneNumber = "8523146978";
+            model.Email = "ayush@gmail.com";
+            model.BookName = "GogteCollege";
+            model.Type = "Family";
+
+           var result = addressBookRepo.AddContactToDatabase(model);
+            Console.WriteLine("Contact added to database : " + result);
         }
     }
 }
