e48ff8fe05522594912b6644eaf6d4bbad1deb57
Chetan2028 <bmchetan2028@gmail.com>
2020-11-28 07:58:26 +0530

    [Chetan] Add . Added contact to datatbase using stored procedure

--- AddressBookADO.NET/AddressBookRepo.cs
+++ AddressBookADO.NET/AddressBookRepo.cs
@@ -259,5 +259,54 @@ namespace AddressBookADO.NET
                 this.connection.Close();
             }
         }
+
+        /// <summary>
+        /// Adds the contact to database.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public bool AddContactToDatabase(AddressBookModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    // Created instance of the given query and connection
+                    SqlCommand command = new SqlCommand("spAddContact", this.connection);
+                    // Command type  as text for stored procedure
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // Adds the values to the stored procedure
+
+                    command.Parameters.AddWithValue("@First_Name", model.FirstName);
+                    command.Parameters.AddWithValue("@Last_Name", model.LastName);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@Zip", model.Zip);
+                    command.Parameters.AddWithValue("@Phone_Number", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Email", model.Email);
+                    command.Parameters.AddWithValue("@AddressBook_Name", model.BookName);
+                    command.Parameters.AddWithValue("@Type", model.Type);
+
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
     }
 }
